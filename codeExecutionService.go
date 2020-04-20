0be47cd68b905ef1c24cb7e6678ee4d4137a5da8
Tancredo Souza <aqueletantan@gmail.com>
2020-04-20 14:38:59 -0300

    feat(CES): execute c++ compiled binary from golang code

--- codeExecutionService.go
+++ codeExecutionService.go
@@ -1,7 +1,20 @@
-package main;
+package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello, world!");
+	cmd := exec.Command("CodeProcessor/execute_cpp_code", "CodeProcessor/input/codeTest.cpp", "CodeProcessor/input/input.txt", "CodeProcessor/input/expected_result.txt")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(out.String())
 }
