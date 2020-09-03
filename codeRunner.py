9f52246c765eb825256e3cb862cf80dd6886c548
Tancredo Souza <aqueletantan@gmail.com>
2020-09-03 14:02:15 -0300

    feat: send different error messages.
    The code evaluation can yield none, compile, execution, timeout or wrong answer errors. This includes an optional error body field, which can be used by the frontend.

--- codeRunner.py
+++ codeRunner.py
@@ -4,8 +4,18 @@ import filecmp
 import random
 import os
 import shutil
+from enum import Enum
 
 CODE_FILENAME = "solution.cpp"
+
+
+class Error(Enum):
+    NO_ERROR = 0
+    COMPILE = 1
+    EXECUTION = 2
+    TIMEOUT = 3
+    WRONG_ANSWER = 4
+
 
 app = Flask(__name__)
 
@@ -13,36 +23,61 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def handleSubmission():
     content = request.get_json()
-    submissionCodeFile = temporarilyStoreCodeFile(content["submitted_code"])
-    submissionDirectory = os.path.dirname(submissionCodeFile)
-    result = compileAndRun(submissionCodeFile)
-    shutil.rmtree(submissionDirectory)
-    return jsonify(result)
+    submissionDir = temporarilyStoreCodeFile(content["submitted_code"])
+    err_type, err_body = compileAndRun(submissionDir)
+    shutil.rmtree(submissionDir)
+    return buildMsg(err_type, err_body)
 
 
 def temporarilyStoreCodeFile(codeText):
-    codeDirectory = f"submission_{random.randint(0,9999999)}"
-    os.mkdir(codeDirectory)
-    codeFilepath = os.path.join(codeDirectory, CODE_FILENAME)
+    codeDir = f"submission_{random.randint(0,9999999)}"
+    os.mkdir(codeDir)
+    codeFilepath = os.path.join(codeDir, CODE_FILENAME)
 
     codeFile = open(codeFilepath, "w")
     codeFile.write(codeText)
     codeFile.close()
-    return codeFilepath
+    return codeDir
 
 
-def compileAndRun(codeFilepath):
-    subprocess.check_call(
-        ('g++', '-o', 'a.out', codeFilepath),
-        stdin=subprocess.DEVNULL)
+def compileAndRun(submissionDir):
+    codeFilepath = os.path.join(submissionDir, CODE_FILENAME)
+    try:
+        subprocess.check_output(
+            ('g++', '-o', 'a.out', codeFilepath),
+            stdin=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        return Error.COMPILE, e.output
 
-    with open('input/input.txt') as infile, open('input/output.txt', 'w') as outfile:
-        subprocess.check_call(
-            ('./a.out',),
-            stdin=infile,
-            stdout=outfile,
-            universal_newlines=True)
-        return filecmp.cmp(outfile.name, "input/expected_result.txt")
+    try:
+        with open('input/input.txt') as infile, open(f'{submissionDir}/output.txt', 'w') as outfile:
+            subprocess.run(
+                './a.out',
+                stdin=infile,
+                stdout=outfile,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+            )
+            return (Error.NO_ERROR if filecmp.cmp("input/expected_result.txt", outfile.name) else Error.WRONG_ANSWER), None
+    except subprocess.TimeoutExpired as e:
+        return Error.TIMEOUT, None
+    except subprocess.CalledProcessError as e:
+        return Error.EXECUTION, e.output
+
+
+def buildMsg(error_type, error_body):
+    return jsonify(
+        err_type="none" if error_type == Error.NO_ERROR
+        else "wa" if error_type == Error.WRONG_ANSWER
+        else "timeout" if error_type == Error.TIMEOUT
+        else "compile" if error_type == Error.COMPILE
+        else "exec" if error_type == Error.EXECUTION
+        else "unknown",
+
+        err_body=error_body.decode(
+            "utf-8") if error_body != None else error_body
+    )
 
 
 if __name__ == '__main__':
